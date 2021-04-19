33dbf98cf05cfdde65d075020ae76d0fb379d665
burdakin <serjb94@gmail.com>
2021-04-19 23:38:23 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var queryObj = null;
+
+async function getQuery(query, page) {
+    let url = `https://openlibrary.org/search.json?q=${query}&page=${page}`;
+    let result = await fetch(url);
+    return await result.json();
+}
+
+async function test() {
+    let query = document.getElementById('search-input').value.toString().toLowerCase();
+    let data = await getQuery(query, 1);
+    queryObj = await data;
+    await console.log(data);
+    await renderTitle();
+}
+
+class BookTitle {
+    constructor(title) {
+        this.title = title;
+    }
+}
+
+function renderTitle() {
+    for (let i = 0; i < queryObj.docs.length; i++) {
+        let title = new BookTitle(queryObj.docs[i].title);
+        let titleElement = document.createElement('div');
+        titleElement.setAttribute('id', `title${i}`);
+        document.getElementById('search-results').appendChild(titleElement);
+        let newTitle = `<input type="radio" class="title-text" id="title-text${i}" name="titles" value="${queryObj.docs[i].cover_i}">
+            <label for="title-text${i}">${title.title}</label>`;
+        document.getElementById(`title${i}`).innerHTML = newTitle;
+        console.log(title);
+    }
+}
