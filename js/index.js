ef9a6f5cfd569291dc12bc02959abd6fd5c2c11e
burdakin <serjb94@gmail.com>
2021-04-20 23:41:32 +0300

    Result list is now rendering

--- js/index.js
+++ js/index.js
@@ -1,18 +1,25 @@
 var queryObj = null;
 
+class Input {
+    constructor() {
+        this.input = document.getElementById('search-input').value.toString().toLowerCase();
+    }
+}
+
 async function getQuery(query, page) {
     let url = `https://openlibrary.org/search.json?q=${query}&page=${page}`;
     let result = await fetch(url);
     return await result.json();
 }
 
-async function test() {
-    let query = document.getElementById('search-input').value.toString().toLowerCase();
-    let data = await getQuery(query, 1);
-    queryObj = await data;
-    await console.log(data);
-    await renderTitle();
+    async function getList(page) {
+        let query = new Input().input;
+        let data = await getQuery(query, page);
+        queryObj = await data;
+        await console.log(data);
+        await renderTitle();
 }
+
 
 class BookTitle {
     constructor(title) {
@@ -30,8 +37,28 @@ function renderTitle() {
             <label for="title-text${i}">${title.title}</label>`;
         document.getElementById(`title${i}`).innerHTML = newTitle;
     }
+    addFooterButtons();
 }
 
-function addFooterButtons (/*текущая страница*/) {
-    //Функция добавления кнопок следующей и предыдущей страницы в футере результатов
+function addFooterButtons() {
+    let page = (queryObj.start / 100) - 1;
+    let back = document.createElement('button');
+    back.setAttribute('id', 'back');
+    back.setAttribute('class', 'back-btn');
+    document.getElementById('search-footer').appendChild(back);
+    document.getElementById('back').innerHTML = 'Вперед';
+
+    document.getElementById('search-footer').addEventListener('click', (e) => {
+        console.log(e);
+
+        if (e.target.id == 'back') {
+            getList(page + 1).then();
+        }
+    })
+
 }
+
+/*
+TODO - Проверить все кавычки;
+
+ */
